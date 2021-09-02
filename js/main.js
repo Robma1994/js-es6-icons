13b96374801bd575dc7454dc8474999c062d8111
Roberto <robertomartinonwg@gmail.com>
2021-09-02 16:08:41 +0200

    Milistone 2 completato

--- js/main.js
+++ js/main.js
@@ -97,26 +97,51 @@ const icons = [
       family: 'fas',
     },
   ];
+const colors = ["Green", "Yellow", "Blue"];
 
 const iconsContainer = document.getElementById('icons');
-containerRepeat(iconsContainer);
 
 
-
+//Milestone 2 - Coloriamo le icone per tipo
+const coloriIcone = colorsIcons(icons,colors);
+containerRepeat(coloriIcone, iconsContainer);
+console.log(coloriIcone)
 
 
 //Funzioni
-//Funzione Milestone 1
-function containerRepeat(array){
-    icons.forEach( (element) => {
-      
-      const {name, family, prefix} = element;
-      array.innerHTML +=`
+//Funzione Milestone 1 (ok)
+function containerRepeat(array, container){
+    array.forEach( (currentValue) => {
+      const {name, family, prefix, color} = currentValue;
+      container.innerHTML +=`
       <div class="card">
-          <i class="${family} ${prefix + name}" ></i>
+          <i class="${family} ${prefix + name}" style="color:${color}" ></i>
           <div class="tit">${name}</div>
       </div> 
       `
   });
 }
-  
+ 
+
+//Funzione Milestone 2a
+function typology(array){
+  const newArrayType = [];
+  array.forEach((currentValue) => {
+    if (newArrayType.includes(currentValue.type) == false){
+      newArrayType.push(currentValue.type);
+    }
+  }); 
+  return newArrayType;
+}
+
+//Funzione Milestone 2b
+function colorsIcons(array, colors) {
+  const types = typology(array);
+
+  const coloriIcone = array.map((currentValue) =>{
+    const positionType = types.indexOf(currentValue.type);
+    currentValue.color = colors[positionType];
+    return currentValue;
+  });
+  return coloriIcone
+}
